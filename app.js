250814fd58ecb0c3b90f006b0ab572a493fd2a0d
Brosarce <creepersgunnacreep@gmail.com>
2015-01-31 13:46:44 -0800

    Began start page

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express()
 
+console.log('Testing, yes, I am here')
+
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.use(express.static(__dirname + '/public'));
+
 
 var server = app.listen(3000, function () {
 
